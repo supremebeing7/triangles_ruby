897f54dd4cdd606a2e895dbb391c2ca9777536ff
gdsfg <fdsfg>
2014-03-10 13:33:22 -0700

    add error checking for sides of 0 length

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -6,14 +6,14 @@ class Triangle
   end
 
   def type_of_triangle
-    if @side_a == @side_b && @side_b == @side_c
-     "equilateral, with dimensions #{@side_a}, #{@side_b}, and #{@side_c}"
-    elsif @side_a + @side_b < @side_c || @side_a + @side_c < @side_b || @side_b + @side_c < @side_a
+    if @side_a== 0 || @side_b == 0 || @side_c == 0 || @side_a + @side_b < @side_c || @side_a + @side_c < @side_b || @side_b + @side_c < @side_a
      "invalid, with dimensions #{@side_a}, #{@side_b}, and #{@side_c}"
+    elsif @side_a == @side_b && @side_b == @side_c
+     "equilateral, with dimensions #{@side_a}, #{@side_b}, and #{@side_c}"
     elsif @side_a == @side_b || @side_b == @side_c || @side_c == @side_a
      "isosceles, with dimensions #{@side_a}, #{@side_b}, and #{@side_c}"
     else
-     "scalene, with dimensions #{@side_a}, #{@side_b}, and #{@side_c}"
+     "scalene, with dimensions #{@side_a}, #{@side_b}, and #{side_c}"
     end
   end
 end
